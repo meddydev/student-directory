2065ce0c1499825e711b27bcc826df77f2e4edeb
Ahmed <ahmed@hallajian.co.uk>
2022-06-07 16:24:56 +0100

    refactored code by defining 3 new methods

--- directory.rb
+++ directory.rb
@@ -1,36 +1,47 @@
+@students = []
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end   
+
+def process(selection)
+    case selection
+        when "1"
+            students = input_students
+        when "2"
+            show_students
+        when "9"
+            exit
+        else
+            puts "I don't know what you meant, try again"
+        end
+end
+
 def interactive_menu
-    students = []
     loop do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit"
-        selection = gets.chomp
-        case selection
-            when "1"
-                students = input_students
-            when "2"
-                print_header
-                print(students)
-                print_footer(students)
-            when "9"
-                exit
-            else
-                puts "I don't know what you meant, try again"
-            end
+        print_menu
+        process(gets.chomp)
     end
 end
 
 def input_students
     puts "Enter name of student"
     puts "To stop, hit return twice"
-    students = []
     name = gets.chomp
     while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    students
+    @students
 end
 
 def print_header
@@ -38,19 +49,19 @@ def print_header
     puts "-------------"
 end
 
-def print(students)
-    students.each do |student|
+def print_students_list
+    @students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(students)
-    if students.count == 0
+def print_footer
+    if @students.count == 0
         puts "No students at Villain Academy"
-    elsif students.count == 1
+    elsif @students.count == 1
         puts "Overall we have 1 great student"
     else
-        puts "Overall we have #{students.count} great students"
+        puts "Overall we have #{@students.count} great students"
     end
 end
 
