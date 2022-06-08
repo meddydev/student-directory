5870a6050846a8a445b7e6cc545c73eb1e603933
Ahmed <ahmed@hallajian.co.uk>
2022-06-08 14:22:43 +0100

    added ability to load file in from command line

--- directory.rb
+++ directory.rb
@@ -1,7 +1,19 @@
 @students = []
 
-def load_students
-    file = File.open("students.csv", "r")
+def try_load_students
+    filename = ARGV.first
+    return if filename.nil?
+    if File.exist?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.count} students from #{filename}"
+    else
+        puts "Sorry, #{filename} doesn't exist."
+        exit
+    end
+end
+
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
     file.readlines.each do |line|
         name, cohort = line.chomp.split(",")
         @students << {name: name, cohort: cohort.to_sym}
@@ -53,18 +65,18 @@ end
 def interactive_menu
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
 def input_students
     puts "Enter name of student"
     puts "To stop, hit return twice"
-    name = gets.chomp
+    name = STDIN.gets.chomp
     while !name.empty? do
         @students << {name: name, cohort: :november}
         puts "Now we have #{@students.count} students"
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
     @students
 end
@@ -90,4 +102,5 @@ def print_footer
     end
 end
 
+try_load_students
 interactive_menu
